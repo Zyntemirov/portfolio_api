f43b7b37c0e87d7652788f44bafafaaefa327183
Azamat Zyntemirov <zyntemirov@gmail.com>
2019-10-05 19:34:26 +0600

    -app urls setting fixed and my firt api ready

--- banner/views.py
+++ banner/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import json
+from django.core import serializers
 
-# Create your views here.
+from django.http import JsonResponse
+
+from .models import Banner
+
+def list_banner_photo(request):
+    banner = Banner.objects.all()
+    json_banner = serializers.serialize('json', banner)
+    json_banner = json.loads(json_banner)
+
+    return JsonResponse({'banner_photos': json_banner})
